346149ba2ad7ff13f7549095231f3f100f441c32
5710110222 <38654887+5710110222@users.noreply.github.com>
2019-02-13 21:05:07 +0700

    hw05

---
+++ ex05/Tunnel.java
@@ -0,0 +1,18 @@
+public class Tunnel{
+    private int slotIn;
+    private int slotOut;
+
+    public Tunnel(int s_in, int s_out){
+        this.slotIn = s_in;
+        this.slotOut = s_out;
+    }
+
+    public int walkIn(int slot){
+        if(slot == slotIn){
+            System.out.println("** Bonut ***Player=" + slotOut); 
+        
+            return slotOut;
+        }
+        return slot;
+    }
+}
